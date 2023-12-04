df2a22495d88708572081af32a45dfba88953db0
FKD13 <44001949+FKD13@users.noreply.github.com>
2023-12-04 17:02:48 +0100

    add day04

---
+++ 04_code.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+part_1() {
+  (
+    sed -E 's/ +/ /g' |
+    sed -E 's/Card [0-9]+:/0-1 |/' |
+    sed -E ':a;s/^([^\|]+ \| ([0-9]+ )*)([0-9]+) (([0-9]+ )*\|( [0-9]+)*) \3(( [0-9]+)*)$/1+\1\4\7/g;ta' |
+    sed -E 's/^([^ ]+) .*$/2^(\1)/' |
+    paste -sd+ |
+    bc 
+  ) < 04_input.txt
+}
+
+part_2() {
+  readarray -t scores <<< "$( 
+    sed -E 's/ +/ /g' 04_input.txt |
+    sed -E 's/Card [0-9]+:/0 |/' |
+    sed -E ':a;s/^([^\|]+ \| ([0-9]+ )*)([0-9]+) (([0-9]+ )*\|( [0-9]+)*) \3(( [0-9]+)*)$/1+\1\4\7/g;ta' |
+    sed -E 's/^([^ ]+) .*$/\1/' |
+    bc
+  )"
+
+  cards="${#scores[@]}"
+ 
+  declare -a card_count
+
+  for ((i = 0; i < "$cards"; i++)); do
+    card_count["$i"]=1
+  done
+
+  sum=0
+  for ((i = 0; i < "$cards"; i++)); do
+    sum+="+${card_count["$i"]}"
+    for ((j = i+1; j <= i + "${scores["$i"]}" && j < "$cards"; j++ )); do
+      card_count["$j"]=$((card_count[j]+card_count[i]))
+    done
+  done
+  bc <<< "$sum"
+}
