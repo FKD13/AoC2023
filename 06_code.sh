adc9c97bdf3b5805268c94e51a648cbf4e221bc3
FKD13 <44001949+FKD13@users.noreply.github.com>
2023-12-07 02:49:29 +0100

    big speedup :o

--- 06_code.sh
+++ 06_code.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
 part_1() {
-  input="$((sed -E 's/ +/ /g' | sed -E 's/^[a-zA-Z]+: //') < 06_input.txt)"
+  input="$((sed -E 's/^[a-zA-Z]+: //;s/ +/ /g' | tr '\n' '-') < 06_input.txt)"
 
-  IFS=' ' read -ra times <<< "$(head -n 1 <<< "$input")"
-  IFS=' ' read -ra dists <<< "$(tail -n 1 <<< "$input")"
+  [[ "$input" =~ ([^-]+)-([^-]+) ]]
+  IFS=' ' read -ra times <<< "${BASH_REMATCH[1]}"
+  IFS=' ' read -ra dists <<< "${BASH_REMATCH[2]}"
 
   for ((i = 0; i < "${#times[@]}"; i++)); do
-    echo "solve(${dists[i]} < (${times[i]}-x)*x; x)"
+    echo "scale=5;(-${times[i]}-sqrt(${times[i]}^2-4*-1*-${dists[i]}))/(2*-1)-(-${times[i]}+sqrt(${times[i]}^2-4*-1*-${dists[i]}))/(2*-1)" 
   done |
-  qalc -t -f - |
-  sed -E 's/^(.*) < x < (.*)$/\2 - \1/' |
   bc |
   xargs printf "%.0f\n" |
   paste -sd'*' |
@@ -18,12 +17,17 @@ part_1() {
 }
 
 part_2() {
-  input="$((sed -E 's/^[a-zA-Z]+: //' | sed -E 's/ +//g') < 06_input.txt)"
+  input="$((sed -E 's/^[a-zA-Z]+: //;s/ +//g' | tr '\n' '-') < 06_input.txt)"
 
-  time="$(head -n 1 <<< "$input")"
-  dist="$(tail -n 1 <<< "$input")"
- 
-  qalc -t "solve($dist < ($time-x)*x; x)" | sed -E 's/^(.*) < x < (.*)$/\2 - \1/' | bc
+  [[ "$input" =~ ([^-]+)-([^-]+) ]]
+  time="${BASH_REMATCH[1]}"
+  dist="${BASH_REMATCH[2]}"
+
+  echo "scale=5;(-${time}-sqrt(${time}^2-4*-1*-${dist}))/(2*-1)-(-${time}+sqrt(${time}^2-4*-1*-${dist}))/(2*-1)" |
+  bc |
+  xargs printf "%.0f\n" |
+  paste -sd'*' |
+  bc
   
   echo "-> Please ceil ◔_◔ <-"
 }
